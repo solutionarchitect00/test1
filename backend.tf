c2f3ec6f23ad7ccc73ff8e7db72a564e06c46938
Ismail <95559246+solutionarchitect00@users.noreply.github.com>
2022-02-25 04:15:46 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "solutionarchitect00mybucket"
     key = "main"
-    region = "ap=south-1"
+    region = "ap-south-1"
     dynamodb_table = "my-dynamodb-table"
   }
 }
