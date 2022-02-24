8bbc18a9efaaa95b9e373493df51a1d4adb2812c
Ismail <95559246+solutionarchitect00@users.noreply.github.com>
2022-02-25 04:15:22 +0530

    Update backend.tf
    bucket name updated

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "mydev-tf-state-bucket"
+    bucket = "solutionarchitect00mybucket"
     key = "main"
-    region = "us-east-2"
+    region = "ap=south-1"
     dynamodb_table = "my-dynamodb-table"
   }
 }
