2f8ec9bc200469f38dca2fdbb5e0aca87bc5b4d0
Ismail <95559246+solutionarchitect00@users.noreply.github.com>
2022-02-25 04:24:39 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "aws_region" {
 
 variable "key_name" { 
     description = " SSH keys to connect to ec2 instance" 
-    default     =  "myJune2021Key" 
+    default     =  "myFebruaryKey" 
 }
 
 variable "instance_type" { 
@@ -15,7 +15,7 @@ variable "instance_type" {
 
 variable "security_group" { 
     description = "Name of security group" 
-    default     = "jenkins-sgroup-dec-2021" 
+    default     = "jenkins-sgroup-feb-2022" 
 }
 
 variable "tag_name" { 
